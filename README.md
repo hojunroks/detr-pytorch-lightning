acbc24d0f2652925aac03f1df3135947260a665c
SNU\hojunroks <hojunroks@gmail.com>
2020-08-26 18:44:31 +0900

    Some initial settings

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # detr-pytorch-lightning
-My reproduction of facebook's DETR using pytorch lightning
+My reproduction of facebook's DETR using pytorch lightning.
+
+Fb has opened their source code but I personally found it a little too difficult, so I decided to implement a simpler version.
+
---
+++ src/detr.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import pytorch_lightning as pl
+from pytorch_lightning import LightningModule
+from .transformer import TransformerEncoder, TransformerDecoder
+
+class DETR(LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.backbone = torchvision.models.resnet50(pretrained=True)
+        self.pos_encoding = PosEncoding()
+        self.encoder = TransformerEncoder()
+        self.decoder = TransformerDecoder()
+
+
+    def forward(self):
+
+        pass
+
+    def configure_optimizers(self):
+        pass
+
+    def training_step(self):
+        pass
